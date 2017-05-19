b11e71575f05aff39a24ba0e55787e469cf21fae
Mario Schuetz <mayo2@gmx.de>
2017-05-19 14:21:26 +0200

    Rename class. Refactor instanciation

--- src/chiffre/Chiffre.java
+++ src/chiffre/Chiffre.java
@@ -4,13 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Chiffre {
 
-	private static Main doStuff;
+	private static Chiffre doStuff;
 	
+	public Chiffre(){
+	}
+
 	public static void main (String[] args) throws IOException{
-    	String userInput = doStuff.userInput();
-        doStuff.analyse(userInput.toLowerCase());
+		doStuff = new Chiffre();
+    	String userInput = doStuff.userInput();  
+    	doStuff.analyse(userInput.toLowerCase());
     }
 	
 	private String userInput() throws IOException {
@@ -28,13 +32,11 @@ public class Main {
 		
 		for(char aChar : charArray){
 			int charArrayPos = 0;
-			if(aChar > 65 && aChar <= 90) charArrayPos = aChar - 65;
+			//if(aChar > 65 && aChar <= 90) charArrayPos = aChar - 65;
 			if(aChar > 97 && aChar <= 122) charArrayPos = aChar - 97;
 			if(aChar == 32) charArrayPos = 26;
 			
 			statistics[charArrayPos]++; 
-		}		
-	}
-
-	
+		}
+	}	
 }
