15d739010a6b1d61e6ebd56c02d2839b8657cad6
Mario Schuetz <mayo2@gmx.de>
2017-05-24 09:59:50 +0200

    Add chiffre example.

--- src/chiffre/Chiffre.java
+++ src/chiffre/Chiffre.java
@@ -3,18 +3,27 @@ package chiffre;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Chiffre {
 
 	private static Chiffre doStuff;
-	
+	private char[] engLetterFreq = {'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'};
+	private static String chiffre = "xureveoulrefknpavberweqwuegwceappergvleovlrazfbevdfaewmedfbwubjvbyenpfalusfeabdensavlvbyenavbecalexwsbeabdevecvppeyvtfeqwueaejwonpfrfeajjwubrewmergfelqlrfoeabdefknwubdergfeajruaperfajgvbylewmergfeysfarefknpwsfsewmergfersurgergfeoalrfsexuvpdfsewmeguoabegannvbfll";
+	private static String text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound the actual teachings of the great explorer of the truth the master builder of human happiness";
 	public Chiffre(){
 	}
 
 	public static void main (String[] args) throws IOException{
 		doStuff = new Chiffre();
-    	String userInput = doStuff.userInput();  
-    	doStuff.analyse(userInput.toLowerCase());
+    	//String userInput = doStuff.userInput();  
+    	int[] charFrequency = doStuff.analyse(chiffre.toLowerCase());
+    	doStuff.decipher(charFrequency, chiffre);
+    	System.out.println("\n");
+    	int[] charFrequency2 = doStuff.analyse(text.toLowerCase());
+    	doStuff.decipher(charFrequency2, text);
+    	
     }
 	
 	private String userInput() throws IOException {
@@ -25,7 +34,7 @@ public class Chiffre {
         return input;
 	}
 	
-	private void analyse(String input){
+	private int[] analyse(String input){
 		
 		char[] charArray = input.toCharArray();
 		int[] statistics = new int[27];
@@ -36,7 +45,28 @@ public class Chiffre {
 			if(aChar > 97 && aChar <= 122) charArrayPos = aChar - 97;
 			if(aChar == 32) charArrayPos = 26;
 			
-			statistics[charArrayPos]++; 
+			statistics[charArrayPos]++;	
 		}
-	}	
+		
+		for(int i : statistics){
+			System.out.print(i + " ");
+		}
+		
+		return statistics;		
+	}
+	
+	private void decipher(int[] charFrequencies, String chiffre){
+		System.out.println("");
+		Arrays.sort(charFrequencies);
+//		for(int i : charFrequencies){
+//			System.out.print(i + " ");
+//		}
+		
+		for (int i : charFrequencies){
+			for(int n = chiffre.length(); n >= 0; n--){
+				
+				System.out.print("");
+			}
+		}
+	}
 }
