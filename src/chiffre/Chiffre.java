7c3c9f76c455a5f00f6bb2e149b1f8b9d79685ba
Mario Schuetz <mayo2@gmx.de>
2017-05-24 10:49:01 +0200

    Make statistics 2d array

--- src/chiffre/Chiffre.java
+++ src/chiffre/Chiffre.java
@@ -18,11 +18,11 @@ public class Chiffre {
 	public static void main (String[] args) throws IOException{
 		doStuff = new Chiffre();
     	//String userInput = doStuff.userInput();  
-    	int[] charFrequency = doStuff.analyse(chiffre.toLowerCase());
-    	doStuff.decipher(charFrequency, chiffre);
+    	int[][] charFrequency = doStuff.analyse(chiffre.toLowerCase());
+    //	doStuff.decipher(charFrequency, chiffre);
     	System.out.println("\n");
-    	int[] charFrequency2 = doStuff.analyse(text.toLowerCase());
-    	doStuff.decipher(charFrequency2, text);
+    	int[][] charFrequency2 = doStuff.analyse(text.toLowerCase());
+    //	doStuff.decipher(charFrequency2, text);
     	
     }
 	
@@ -34,35 +34,40 @@ public class Chiffre {
         return input;
 	}
 	
-	private int[] analyse(String input){
+	private int[][] analyse(String input){
+		
+		int alphabetSize = 27;
 		
 		char[] charArray = input.toCharArray();
-		int[] statistics = new int[27];
+		int[][] statistics = new int[2][alphabetSize];
 		
 		for(char aChar : charArray){
 			int charArrayPos = 0;
 			//if(aChar > 65 && aChar <= 90) charArrayPos = aChar - 65;
-			if(aChar > 97 && aChar <= 122) charArrayPos = aChar - 97;
-			if(aChar == 32) charArrayPos = 26;
+			if(aChar >= 97 && aChar <= 122) charArrayPos = aChar - 97; // a - z
+			if(aChar == 32) charArrayPos = 26; //space
 			
-			statistics[charArrayPos]++;	
+			statistics[1][charArrayPos]++;	
 		}
-		
-		for(int i : statistics){
-			System.out.print(i + " ");
+		System.out.println("Analysis: ");
+		for(int i = 0; i < alphabetSize; i++){
+			char currSign = 0;
+			if(i == alphabetSize -1) currSign = 32;
+			else currSign = (char) (i + 97);
+			System.out.println(currSign + " "  + statistics[1][i] );			
 		}
 		
 		return statistics;		
 	}
 	
-	private void decipher(int[] charFrequencies, String chiffre){
+	private void decipher(int[][] charFrequency, String chiffre){
 		System.out.println("");
-		Arrays.sort(charFrequencies);
+		Arrays.sort(charFrequency);
 //		for(int i : charFrequencies){
 //			System.out.print(i + " ");
 //		}
 		
-		for (int i : charFrequencies){
+		for (int[] i : charFrequency){
 			for(int n = chiffre.length(); n >= 0; n--){
 				
 				System.out.print("");
